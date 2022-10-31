e73c1e8284c71702ff7679d0fb7717d78b94efaa
Genison Wibawa <wibawagenisonb@gmail.com>
2022-10-31 16:32:18 +0800

    images2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="Singapore"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,23 @@
-function Card() {
+import star from "../assets/star.png"
+import katie from "../assets/katie-zaferes.png"
+
+function Card(props) {
+  let img = "../assets/" + props.img
   return (
     <div className="card">
-      <img src="../images/katie-zaferes.png" className="card--image" />
+      <img src={katie} className="card--image" />
       <div className="card--stats">
-        <img src="../images/star.png" className="card--star" />
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
+        <img src={star} className="card--star" />
+        <span>
+          {props.rating}
+          {img}
+        </span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
+      <p>{props.title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   )
