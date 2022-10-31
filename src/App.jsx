fa57e37ab68fdd229eff296cb0543fdc42d5ff78
Genison Wibawa <wibawagenisonb@gmail.com>
2022-10-31 15:55:35 +0800

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero"
+import Card from "./Components/Card"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
